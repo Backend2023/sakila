d2181f63fc45d09ff92495ca75132cc9fa52746b
mrvic <mrvic.predrag@gmail.com>
2024-04-10 21:05:16 +0200

    group middleware route

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+Route::get('/', function () {  //anonimna funkcija, closure
+    return view('welcome');
+});
+*/
+
 Route::view('/', 'welcome');
 
+Route::middleware(['auth', 'verified'])->group(function () {
 Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
+   // ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+    Route::view('dash', 'dashboard')  //uri, view
+   // ->middleware(['auth', 'verified']) // koristi auth middleware, samo logirani
+    ->name('dashboard'); // svaku rutu je pozeljno imenovati
+});
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
